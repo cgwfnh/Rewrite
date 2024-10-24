8b308a7cce303e2330c5e49ba75f1f3bc51aa6ff
cgwfnh <cgwfnh@qq.com>
2024-10-24 21:02:19 +0800

    Update goodnotes6.js

--- goodnotes6.js
+++ goodnotes6.js
@@ -9,11 +9,11 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/cgwfnh/Rewrite/main/goodnotes6.js
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/cgwfnh/Rewrite/main/goodnotes6.js
+^https:\/\/isi\..*\.g.*\.(com\..*|com)\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/cgwfnh/Rewrite/main/goodnotes6.js
+^https:\/\/isi\..*\.g.*\.(com\..*|com)\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/cgwfnh/Rewrite/main/goodnotes6.js
 
 [mitm]
-hostname = isi.csan.goodnotes.com
+hostname = isi.*.g*.com*
 
 *************************************/
 
