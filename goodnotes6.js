00a9d354a85eb562c9a2d82414567f3dabc69da5
cgwfnh <cgwfnh@qq.com>
2024-10-26 17:33:17 +0800

    Update goodnotes6.js

--- goodnotes6.js
+++ goodnotes6.js
@@ -35,23 +35,23 @@ if (typeof $response == "undefined") {
 } else if (chxm1024 && chxm1024.subscriber) {
   data = {
  "Author": "chxm1023",
- "Telegram" : "https://t.me/chxm1023",
+ "Telegram": "https://t.me/chxm1023",
  "warning": "仅供学习，禁止转载或售卖",
  "purchase_date": "2024-09-09T09:09:09Z"
  };
   chxm1024.subscriber.subscriptions[(jsid)] = {
  "Author": "chxm1023",
- "Telegram" : "https://t.me/chxm1023",
+ "Telegram": "https://t.me/chxm1023",
  "warning": "仅供学习，禁止转载或售卖",
  "original_purchase_date": "2024-09-09T09:09:09Z",
  "purchase_date": "2024-09-09T09:09:09Z",
- "store" : "app_store",
+ "store": "app_store",
  "ownership_type": "PURCHASED"
  };
   chxm1024.subscriber.entitlements[(namea)] = JSON.parse(JSON.stringify(data));
   chxm1024.subscriber.entitlements[(nameb)] = JSON.parse(JSON.stringify(data));
-  chxm1024.subscriber.entitlements[(namea)].product_identifier = (jsid);
-  chxm1024.subscriber.entitlements[(nameb)].product_identifier = (jsid);
+  chxm1024.subscriber.entitlements[(namea)].product_identifier = jsid;
+  chxm1024.subscriber.entitlements[(nameb)].product_identifier = jsid;
 } else if (chxm1024 && chxm1024.identity) {
   chxm1024.identity.metadata_public[(maxfile)] = JSON.parse(JSON.stringffy(65535));
 }
