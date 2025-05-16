d8ddbd318862a6ec1ce5f86a9a220d580ebf20ca
cgwfnh <cgwfnh@qq.com>
2025-05-16 12:23:28 +0800

    Update nb4759.js

--- nb4759.js
+++ nb4759.js
@@ -10,15 +10,12 @@ hostname = *.nobook.com
 
 // 核心功能：修改响应JSON，解锁会员
 (() => {
-  const response = JSON.parse($response.body);
-  //console.log(JSON.stringify(response));
-  // 修改isVip
-  function modifyVip(obj) {
-    response.replace(/isVip:\w/g, 'isVip:true');
-    }
+  function modify(response) {
+  if (typeof response === 'string') {
+      return response.replace(/isVip:\w/gi, 'isVip:true');
   }
-  
-  modifyVip(response); // 执行修改
-  //console.log(JSON.stringify(response));
-  $done({ body: JSON.stringify(response) }); // 返回修改后的响应
+  return response;
+}
+
+$done({ body: modify($response.body) }); // 返回修改后的响应
 })();
