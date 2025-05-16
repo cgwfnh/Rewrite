6fe1fae5da52316422a3113889488bd6d06f5e13
cgwfnh <cgwfnh@qq.com>
2025-05-16 11:48:42 +0800

    Create nb4759.js

---
+++ nb4759.js
@@ -0,0 +1,24 @@
+/*
+ * NB实验室
+ * 解锁会员
+ * 
+[rewrite_local]
+^https?:\/\/app\-v3(-qn)\.nobook\.com\/app\-resource\/*\/*\/4759.js url script-response-body https://raw.githubusercontent.com/cgwfnh/Rewrite/main/nb4759.js
+[mitm]
+hostname = *.nobook.com
+*/
+
+// 核心功能：修改响应JSON，解锁会员
+(() => {
+  const response = JSON.parse($response.body);
+  //console.log(JSON.stringify(response));
+  // 修改isVip
+  function modifyVip(obj) {
+    response.replace(/isVip:\w/g, 'isVip:true');
+    }
+  }
+  
+  modifyVip(response); // 执行修改
+  //console.log(JSON.stringify(response));
+  $done({ body: JSON.stringify(response) }); // 返回修改后的响应
+})();
